5acf1e8af86683e5265818be06217f312510310d
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-12-15 17:54:19 +0530

    Add files via upload

---
+++ RemoveAll.java
@@ -0,0 +1,29 @@
+package com.Xworkz.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RemoveAll {
+
+	public static void main(String[] args) {
+		Collection arrayList = new ArrayList();
+
+		arrayList.add("DBMS");
+		arrayList.add("JAVA");
+		arrayList.add("Operating System");
+		arrayList.add("Digital Electronics");
+		arrayList.add("Sotware Engg");
+	
+		
+		Collection arrayList1=new ArrayList();
+		
+		arrayList1.add("DBMS");
+		arrayList1.add("JAVA");
+		
+		System.out.println(arrayList.size());
+		arrayList.removeAll(arrayList1);
+		System.out.println(arrayList.size());
+
+	}
+
+}
